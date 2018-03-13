daa677430e8a9541e70aa113d0c256e1d4b58d6b
Vorathel <mornos70@gmail.com>
2018-03-13 19:46:43 +0100

    messagebox added

--- pytutorial2.py
+++ pytutorial2.py
@@ -1,10 +1,20 @@
 #!/usr/bin/python3
 import tkinter as tk
+from tkinter import messagebox as tkMessageBox
+
+tkMessageBox.askyesno("Choose")
 
 
 top = tk.Tk()
 
 w = tk.Label(top,text='Hello World')
 w.pack()
+
+f=open('test.txt','r')		#
+for line in f:			#Schreibt ersten 
+	print (line[0])		#Buchstaben aus test.txt 
+
+f.close()			#in terminal
+
 
 top.mainloop()
